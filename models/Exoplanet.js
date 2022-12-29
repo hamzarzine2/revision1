e9fa514cee759e120eaf84c2f62affeee99b2024
hamzarzine2 <hamza.rzine@student.vinci.be>
2022-12-29 14:47:56 +0100

    build(package.json): installing prettier,eslint,jest,commitizen

--- models/Exoplanet.js
+++ models/Exoplanet.js
@@ -1,26 +1,29 @@
-
 const exoplanetsTable = [
-  { uniqueName: "TRAPPIST-1-d", hClass: "Mésoplanète", discoveryYear: 2016 },
-  { uniqueName: "KOI-1686.01", hClass: "Mésoplanète", discoveryYear: 2011 },
-  { uniqueName: "LHS 1723 b", hClass: "Mésoplanète", discoveryYear: 2017 },
+    { uniqueName: "TRAPPIST-1-d", hClass: "Mésoplanète", discoveryYear: 2016 },
+    { uniqueName: "KOI-1686.01", hClass: "Mésoplanète", discoveryYear: 2011 },
+    { uniqueName: "LHS 1723 b", hClass: "Mésoplanète", discoveryYear: 2017 },
 ];
 
 module.exports.list = () => exoplanetsTable;
 
 module.exports.save = (data) => {
-  exoplanetsTable.push(data);
+    exoplanetsTable.push(data);
 };
 
 module.exports.search = (uniqueName) => {
-  // Initialiser le tableau à vide sinon -> appel push -> cannot read property of null
-  const exoplanetList = [];
-  for (let exoplanet of exoplanetsTable) {
-    if (exoplanet.uniqueName.toUpperCase().startsWith(uniqueName.toUpperCase())) {
-      console.log("trouvé " + JSON.stringify(exoplanet));
-      exoplanetList.push(exoplanet)
+    // Initialiser le tableau à vide sinon -> appel push -> cannot read property of null
+    const exoplanetList = [];
+    for (let exoplanet of exoplanetsTable) {
+        if (
+            exoplanet.uniqueName
+                .toUpperCase()
+                .startsWith(uniqueName.toUpperCase())
+        ) {
+            console.log("trouvé " + JSON.stringify(exoplanet));
+            exoplanetList.push(exoplanet);
+        }
     }
-  }
-  return exoplanetList;
+    return exoplanetList;
 };
 
 // On peut également écrire :
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-
-
-// déclenchement du middleware uniquement pour /users et la suite 
+// déclenchement du middleware uniquement pour /users et la suite
 router.use((req, res, next) => {
-  console.log('Exo ExoPlanet Users ...')
-  next();
+    console.log("Exo ExoPlanet Users ...");
+    next();
 });
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
+router.get("/", (req, res, next) => {
+    res.send("respond with a resource");
 });
-
-
 
 module.exports = router;
